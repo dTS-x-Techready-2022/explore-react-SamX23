688b2d15452912044d10c637bb56814f0dd5f0fb
Sami Kalammallah <skalexsong@gmail.com>
2022-10-01 06:55:19 +0700

    Update : add and implement material ui

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ErrorPage from "./error-page";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 const router = createBrowserRouter([
   {
