627143fb24d759f27211b113871b0f186c42f3e8
Sami Kalammallah <skalexsong@gmail.com>
2022-10-01 07:04:19 +0700

    Update : add redux example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
+import { Counter } from './features/Counter';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
